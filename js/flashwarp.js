115f7843809e7762677701ee34743f500070537f
Artem Abashev <artem@abashev.com>
2014-01-26 22:00:39 +0400

    Initial import.

---
+++ js/flashwarp.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function (window) {
+    var FlashWarpGate = function (params) {
+        var selector = window.document.querySelectorAll(params),
+		    i = 0;
+
+        return this;
+    };
+
+    var FlashWarp = function (params) {
+        return new FlashWarpGate(params);
+    };
+    
+    FlashWarp.fn = FlashWarpGate.prototype =
+	{
+		
+	};
+    
+    if (!window.FlashWarp)
+        window.FlashWarp = FlashWarp;
+}) (window);
---
+++ readme.md
@@ -0,0 +1,1 @@
+Flash-JavaScript bridge on steroids
