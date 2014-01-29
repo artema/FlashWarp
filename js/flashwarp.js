3abb30e2e212d2d5a455d94d444b06e9aa535875
Artem Abashev <artem@abashev.com>
2014-01-29 22:34:27 +0400

    JavaScript functions mapping.

--- js/flashwarp.js
+++ js/flashwarp.js
@@ -1,20 +1,48 @@
 'use strict';
 
 (function (window) {
-    var FlashWarpGate = function (params) {
-        var selector = window.document.querySelectorAll(params),
-		    i = 0;
+    var FlashWarpGate = function (id) {
+        this.flashObject = window.document.getElementById(id);
 
+        if (!this.flashObject)
+            throw new Error("Element " + id + " not found");
+        
+        this.token = id;
+        this.functionsMap = {};
+        
         return this;
     };
 
-    var FlashWarp = function (params) {
-        return new FlashWarpGate(params);
-    };
+    var flashWarpMap = {};
     
-    FlashWarp.fn = FlashWarpGate.prototype =
+    var FlashWarp = function (id, command)
+    {        
+        var gate = flashWarpMap[id] || (flashWarpMap[id] = new FlashWarpGate(id));
+        
+        if (command)
+        {                  
+            gate[command.operation].apply(gate, command.arguments);
+        }
+        
+        return gate;
+    };
+
+    FlashWarpGate.prototype =
 	{
-		
+        token: null,
+		flashObject: null,
+        functionsMap: null,
+        
+        mapFunction: function (name, handler)
+        {
+            this.functionsMap[name] = handler;
+            return this;
+        },
+        
+        execFunction: function (name, args)
+        {
+            this.functionsMap[name].call(null, args);
+        }
 	};
     
     if (!window.FlashWarp)
